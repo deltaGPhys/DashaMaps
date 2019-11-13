42ecfe23e43cdd8e6f4e0dde8ee6d7bfe023a1d6
deltagphys <cty.jgates@gmail.com>
2019-11-13 17:38:20 -0500

    first pass - need to expand the node type

--- src/main/map/HashMapX.java
+++ src/main/map/HashMapX.java
@@ -1,23 +1,49 @@
 package map;
 
 
+import java.util.ArrayList;
+
 public class HashMapX<E extends Comparable<E>,T> {
 
     private static Hasher HASHER;
     private static int hashPoss;
-    private SinglyLinkedList<E>[] lists;
+    private static Object[] hashVals;
+    private ArrayList<SinglyLinkedList<E>> lists;
 
 
-    public HashMapX() {
+    public HashMapX() throws NoHashingFunctionException {
         if (HASHER == null) {
-
+            throw new NoHashingFunctionException();
         }
+    }
+
+    public boolean put(E element, T value)  {
+        SinglyLinkedList<E> targetList;
+        try {
+            targetList = lists.get(findListIndex(element));
+            targetList.add(element);
+            return true;
+        } catch (HashValueNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public int findListIndex(E element) throws HashValueNotFoundException {
+        E hashValue = (E) HASHER.hashFunction(element);
+        for (int i = 0; i < hashPoss; i++) {
+            if (hashValue.equals((E)hashVals[i])) {
+                return i;
+            }
+        }
+        throw new HashValueNotFoundException();
     }
 
     public void changeHashingFunction(Hasher<E> hasher) {
         HASHER = hasher;
         hashPoss = hasher.getBinNum();
-        lists = new SinglyLinkedList<E>[hashPoss];
-
+        hashVals = hasher.getHashVals();
+        lists = new ArrayList<SinglyLinkedList<E>>(hashPoss);
     }
 }
